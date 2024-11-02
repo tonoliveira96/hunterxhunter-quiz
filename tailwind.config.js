d4c89f62fa4705b13bb8e663c8c0cd7dc7028884
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-11-01 22:51:45 -0300

    feat: Configurado Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors:{
+      'hxh-green': '#3A8E3A',
+      'hxh-blue': '#3C5AA6',
+      'hxh-dark-blue': '#2C3E5F',
+      'hxh-cream': '#F5E3C8',
+      'white': '#FFFFFF'
+    }
+  },
+  plugins: [],
+}
+
