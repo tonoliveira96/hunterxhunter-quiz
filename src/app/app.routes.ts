564ee4f739bf9f6e711926551b39d36d5aca00ed
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-11-01 23:34:56 -0300

    feat: Finalizado estilos da Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SurveyComponent } from './pages/survey/survey.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'survey',
+    component: SurveyComponent
+  }
+];
